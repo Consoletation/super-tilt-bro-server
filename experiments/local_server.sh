f4264faeb0def3e12ce1d048766557f9d5b16a05
Daniel Porter <dpreid@gmail.com>
2023-11-16 15:51:26 +0000

    Install python package in venv and use entrypoints

--- experiments/local_server.sh
+++ experiments/local_server.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
+# Determine the directory of this script
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+BASE_DIR="${SCRIPT_DIR}/.."
+
+# Create and activate a virtualenv if not already done
+if [ ! -d "${BASE_DIR}/python/.venv" ]; then
+    python3 -m virtualenv "${BASE_DIR}/python/.venv"
+fi
+source "${BASE_DIR}/python/.venv/bin/activate"
+pip install "${BASE_DIR}/python"
+
 mkdir -p /tmp/stb_games
 STB_GAME_SUMMARIES=/tmp/stb_games ../src/server/stb_server &
 server_pid=$!
-../python/login_server/login_server.py --db-file /tmp/stb_login.json --log-file /tmp/stb_login.log &
+stb-login-server --db-file /tmp/stb_login.json --log-file /tmp/stb_login.log &
 login_pid=$!
-../python/ranking_server/ranking_server.py --db-file /tmp/stb_ranking.json --log-file /tmp/stb_ranking.log &
+stb-ranking-server --db-file /tmp/stb_ranking.json --log-file /tmp/stb_ranking.log &
 ranking_pid=$!
 mkdir -p /tmp/stb_replay
-../python/replay_server/replay_server.py --db-file /tmp/stb_replay.json --replay-dir /tmp/stb_replay --log-file /tmp/stb_replay.log --bmov-to-fm2 ../tools/bmov_to_fm2 &
+stb-replay-server --db-file /tmp/stb_replay.json --replay-dir /tmp/stb_replay --log-file /tmp/stb_replay.log --bmov-to-fm2 ../tools/bmov_to_fm2 &
 replay_pid=$!
 
 echo "PIDs"
