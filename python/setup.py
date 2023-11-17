d90dc42310e6edd3de317501426d62e8b5148812
Daniel Porter <dpreid@gmail.com>
2023-11-17 00:51:41 +0000

    WIP pybind11 changes to package environment

---
+++ python/setup.py
@@ -0,0 +1,25 @@
+"""Setup file for the Python package."""
+
+from __future__ import annotations
+
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+from setuptools import setup
+
+ext_modules = [
+    Pybind11Extension(
+        "bmov_to_fm2",
+        [
+            "cpp/src/GameState.cpp",
+            "cpp/tools/bmov_to_fm2.cpp",
+        ],
+        include_dirs=[
+            "cpp/src",
+            "cpp",
+        ],
+        define_macros=[("PYBIND11", "1")],
+        extra_compile_args=["-flto"],
+        extra_link_args=["-luuid", "-lz"],
+    ),
+]
+
+setup(cmdclass={"build_ext": build_ext}, ext_modules=ext_modules)
